dd40cf0fbfa19d5faf786f034056d86cb75f1231
weslley182 <weslley182>
2015-03-28 22:19:46 -0300

    COrreção Abstract

--- AbstractFactory/Classes/OracleConnection.cs
+++ AbstractFactory/Classes/OracleConnection.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
 namespace AbstractFactory.Classes
-{  
+{
     // Produto Concreto (Concret Product)
-    public class SQLConnection : DBConexao
+    public class OracleConnection : DBConexao
     {
         public override void Open()
         {
-            Console.WriteLine("Método Open de SQLConnection foi chamado");
+            Console.WriteLine("Método Open de OracleConnection foi chamado");
         }
     }
 }
