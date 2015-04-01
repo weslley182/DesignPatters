04646b64d6e855f5f835e25d8b8b88cf0041dbc2
weslley182 <weslley182>
2015-03-31 22:56:42 -0300

    Singleton

---
+++ Singleton/Classes/ConexaoBD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton.Classes
+{
+    // Singleton
+    public class ConexaoBD
+    {
+        // guarda internamente a instância        
+        private static ConexaoBD _instance;
+        // esconde construtor
+        protected ConexaoBD()
+        {
+            //Nao consegue usar o new mais
+        }
+
+        // método que retorna singleton
+        public static ConexaoBD Instance()
+        {
+            // garantir que sempre uma instância exista, só uma
+            if (_instance == null)
+            {
+                _instance = new ConexaoBD();
+            }
+
+            return _instance;
+        }
+
+        public string stringConexao { get; set; }
+
+        public void Open()
+        {
+            Console.WriteLine("Abrindo conexão com banco " + stringConexao);
+        }
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Singleton.Classes;
+using System;
 
 namespace Singleton
 {
+    // Client
     class Program
     {
         static void Main(string[] args)
         {
+            var con1 = ConexaoBD.Instance();
+            con1.stringConexao = "SQL Server";
+            con1.Open();
+
+            var con2 = ConexaoBD.Instance();
+            con2.Open();
+
+            // comprovando que são a mesma instância
+            if (con1 == con2) // está referenciado a mesma posição de memória, apontando para o mesmo objeto?
+                Console.WriteLine("São a mesma instância");
+            Console.ReadLine();
         }
     }
 }
